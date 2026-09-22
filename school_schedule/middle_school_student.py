f358049891ac55c9f8f4c6203b3e27e0b8032635
Hardeep Kaur <hardeep1107@gmail.com>
2026-09-22 11:12:03 -0700

    Added Test Cases for Middle School Student

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,18 +1,6 @@
 from .student import Student
 
 # add MiddleSchoolStudent here
-'''
-We would like to derive a MiddleSchoolStudent child class. MiddleSchoolStudent will have all the attributes and behaviors of Student, but it will also:
-
-Track whether the student receives school transportation using the boolean attribute gets_transportation (can be set in the constructor, defaults to False)
-Update the summary method to include information about the student's transportation status
-In the main.py, create an instance of a MiddleSchoolStudent and add it to the student list. Make sure its summary gets printed out.
-Include tests for the additional functionality
-There is one test provided (currently commented out)
-Uncomment the test and implement the MiddleSchoolStudent class so that it passes
-Implement additional tests for the MiddleSchoolStudent class (review the High School Student class for ideas)
-
-'''
 
 class MiddleSchoolStudent(Student):
     def __init__(self, name, grade, classes, gets_transportation=False):
